c3809d850d847a8daaca6fe96c38dc318fb22c9d
Mathew Carter <mathewcarter@Mathews-MacBook-Air.local>
2024-12-24 23:52:17 -0500

    populate database with all data

--- data/populate.py
+++ data/populate.py
@@ -10,16 +10,17 @@ def main():
     """
     get data from data.csv and populate database
     """
-    rows = []
+    games = []
     # open and read data.csv
     with open("data.csv", mode="r", encoding="utf-8") as file:
         csv_file = csv.reader(file)
         for i, line in enumerate(csv_file):
-            if i in (0, 1):
+            # skip the title line
+            if i == 0:
                 continue
             # * is the "splat" operator, used for unpacking arguments
             game = Game(*line[:10])
-            rows.append(game)
+            games.append(game)
 
     conn = psycopg2.connect(
         database="postgres",
@@ -29,43 +30,108 @@ def main():
         port=5432,
     )
     cursor = conn.cursor()
-    create_schema = """
-        CREATE SCHEMA underdog;
-    """
-    create_teams = """
-        CREATE TABLE underdog.teams (
-            name VARCHAR NOT NULL PRIMARY KEY
-        );
-    """
-    create_sportsbook = """
-        CREATE TABLE underdog.sportsbook (
-            name VARCHAR NOT NULL PRIMARY KEY
-        );
-    """
-    create_games = """
-        CREATE TABLE underdog.games (
-            home_team_name VARCHAR REFERENCES underdog.teams(name),
-            away_team_name VARCHAR REFERENCES underdog.teams(name),
-            year INTEGER,
-            sportsbook VARCHAR REFERENCES underdog.sportsbook(name),
-            home_team_moneyline INTEGER,
-            away_team_moneyline INTEGER,
-            home_team_score INTEGER,
-            away_team_score INTEGER,
-            PRIMARY KEY (home_team_name, away_team_name, year)
-        );
-    """
-    commands = [create_schema, create_teams, create_sportsbook, create_games]
-    for command in commands:
-        cursor.execute(command)
+    try:
+        # create db tables
+        create_schema = """
+            CREATE SCHEMA underdog;
+        """
+        create_teams = """
+            CREATE TABLE underdog.teams (
+                name VARCHAR NOT NULL PRIMARY KEY
+            );
+        """
+        create_sportsbook = """
+            CREATE TABLE underdog.sportsbooks (
+                name VARCHAR NOT NULL PRIMARY KEY
+            );
+        """
+        create_regions = """
+            CREATE TABLE underdog.regions (
+                region VARCHAR NOT NULL PRIMARY KEY
+            );
+        """
+        create_games = """
+            CREATE TABLE underdog.games (
+                home_team_name VARCHAR REFERENCES underdog.teams(name),
+                away_team_name VARCHAR REFERENCES underdog.teams(name),
+                year INTEGER,
+                sportsbook VARCHAR REFERENCES underdog.sportsbooks(name),
+                home_team_moneyline INTEGER,
+                away_team_moneyline INTEGER,
+                home_team_score INTEGER,
+                away_team_score INTEGER,
+                round INTEGER,
+                region VARCHAR REFERENCES underdog.regions(region),
+                PRIMARY KEY (home_team_name, away_team_name, year)
+            );
+        """
+        commands = [
+            create_schema,
+            create_teams,
+            create_sportsbook,
+            create_regions,
+            create_games,
+        ]
+        for command in commands:
+            cursor.execute(command)
 
-    # Fetch all rows from database
-    # record = cursor.fetchall()
-    # print("Data from Database:- ", record)
+        # populate tables
+        # 1. get unique sportsbooks, team names, and regions
+        books = set()
+        teams = set()
+        regions = set()
+        game: Game
+        for game in games:
+            books.add(game.sportsbook)
+            teams.add(game.home_team_name)
+            teams.add(game.away_team_name)
+            regions.add(game.region)
+        insert_book = """
+            INSERT INTO underdog.sportsbooks (name) VALUES (%s);
+        """
+        for name in books:
+            cursor.execute(insert_book, (name,))
+
+        insert_team = """
+            INSERT INTO underdog.teams (name) VALUES (%s);
+        """
+        for name in teams:
+            cursor.execute(insert_team, (name,))
+
+        insert_region = """
+            INSERT INTO underdog.regions (region) VALUES (%s);
+        """
+        for name in regions:
+            cursor.execute(insert_region, (name,))
+
+        # 2. insert games into database
+        insert_game = """
+            INSERT INTO underdog.games (home_team_name, away_team_name, year, sportsbook, home_team_moneyline, away_team_moneyline, home_team_score, away_team_score, round, region)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        for game in games:
+            cursor.execute(
+                insert_game,
+                (
+                    game.home_team_name,
+                    game.away_team_name,
+                    game.year,
+                    game.sportsbook,
+                    game.home_team_moneyline,
+                    game.away_team_moneyline,
+                    game.home_team_score,
+                    game.away_team_score,
+                    game.round,
+                    game.region,
+                ),
+            )
+        print("Populated database successfully!")
+    except psycopg2.DatabaseError as e:
+        print("Error creating database:", e)
+
     conn.commit()
     conn.close()
     cursor.close()
-
 
 
 class Game:
